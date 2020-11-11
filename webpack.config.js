3b89adabc664087349d316e04b7d4bf50b4ffe25
Fabian Yee <fabianyee952@gmail.com>
2020-11-11 15:39:26 -0800

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  entry: `${__dirname}/public/src/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: `${__dirname}/public/dist`
+  },
+  module: {
+    rules: [
+      {
+        test:/\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  watch: true
+}
