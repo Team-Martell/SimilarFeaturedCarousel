8a58ef63eb58a537a583c0d1c7b6dbbc70f609c5
Fabian Yee <fabianyee952@gmail.com>
2020-11-17 11:38:14 -0800

    Refactor and Implement arrows functionality

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -9,26 +9,102 @@ class Carousel extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      size: Math.ceil(this.props.data.length / 7)
+      size: Math.ceil(this.props.data.length / 7),
+      currentIndex: 0,
+      left: false,
+      right: true,
+      xPosition: 0
+    }
+  }
+
+  move(direction) {
+    var x = this.state.xPosition;
+    var dif = 1407;
+    if (direction === 'left') {
+      x += dif;
+    } else {
+      x -= dif;
+    }
+    this.setState({
+      xPosition: x
+    })
+  }
+
+  goNext() {
+    var index = this.state.currentIndex;
+    var length = this.state.size;
+    var lastIndex = length - 1;
+    if (index < lastIndex) {
+      this.move('right');
+
+      index++;
+      this.setState({
+        currentIndex: index,
+      }, () => {
+        if (this.state.currentIndex === this.state.size - 1) {
+          console.log('last arrived');
+          this.setState({
+            right: false,
+          });
+        }
+        if (this.state.currentIndex === 1) {
+          console.log('second arrived')
+          this.setState({
+            left: true
+          });
+        }
+      });
+    }
+  }
+
+  goPrev() {
+    var index = this.state.currentIndex;
+    var length = this.state.size;
+    var x = this.state.xPosition;
+
+    if (index > 0) {
+      index--;
+      this.move('left');
+      this.setState({
+        currentIndex: index,
+      }, () => {
+
+        if (this.state.currentIndex === this.state.size - 2) {
+          this.setState({
+            right: true
+          })
+        }
+
+        if (this.state.currentIndex === 0) {
+          this.setState({
+            left: false
+          })
+        }
+      });
     }
   }
 
 
   render() {
     return (
-      <styles.Carousel>{this.state.size}
+      <styles.Carousel>
         <Arrow
           direction='left'
-          display={false}
+          display={this.state.left}
+          onClick={this.goPrev.bind(this)}
+          index={this.state.currentIndex}
         />
-        <styles.ImageList>
+        <styles.ImageList x={this.state.xPosition}>
           {this.props.data.map((item, i) => {
             return (<Image data={item} key={i}/>);
           })}
         </styles.ImageList>
+
         <Arrow
           direction='right'
-          display={true}
+          display={this.state.right}
+          onClick={this.goNext.bind(this)}
+          index={this.state.currentIndex}
         />
       </styles.Carousel>
     )
